12aa2cafeea32426c49e4ab71885863ba588f572
ithan <ithan.camacho@epn.edu.ec>
2025-05-21 14:33:35 -0500

    cambios en el archivo app.routes.ts y main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { IonicModule } from '@ionic/angular';
 import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideHttpClient(),
+    provideRouter(routes),
   ],
 });
